13d49f84542e00cbbe245a0ac5d3edbb82f0588d
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-06-07 16:49:54 +0530

    Time: 201 ms (58.02%), Space: 30 MB (40.02%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int> > res = {};
-        int n = nums.size();
-        
         sort(nums.begin(), nums.end());
-        if(nums[0]>0)
-            return {};
+        int n = nums.size();
+        vector<vector<int> > res = {};
         
-        for(int i=0;i<n-2;i++)//So that we get at least 3 elements
+        for(int i=0;i<n-2;i++)
         {
-            if(nums[i]>0)//If the first element is positive then surely we won't get -ve up ahead since the list is sorted
+            if(nums[i]>0)
                 break;
-            if(i==0||nums[i]!=nums[i-1])//Condition because we have to return unique pairs
+            
+            int num = nums[i];//Using this as one of the elements
+            if(i==0||nums[i-1]!=nums[i])
             {
-                int lo = i+1, hi = n-1;
+                int sum = 0-num;
+                int lo = i+1;
+                int hi = n-1;
+                
                 while(lo<hi)
                 {
-                    int sum = 0-nums[i];
                     if(nums[lo]+nums[hi]==sum)
                     {
                         vector<int> temp;
-                        temp.push_back(nums[i]);
+                        temp.push_back(num);
                         temp.push_back(nums[lo]);
                         temp.push_back(nums[hi]);
                         res.push_back(temp);
                         
-                        while(lo<hi && nums[lo]==nums[lo+1])//So that we skip same numbers
+                        while(lo<hi && nums[lo]==nums[lo+1])
                             lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1])//So that we skip same numbers
+                        while(lo<hi && nums[hi]==nums[hi-1])
                             hi--;
+                        
                         lo++;
-                        hi--;
+                        hi--;//for moving ahead of the pointers
                     }
-                    else if(nums[lo]+nums[hi]<sum)
-                    {
+                    else if(nums[lo]+nums[hi] < sum)
                         lo++;
-                    }
-                    else if(nums[lo]+nums[hi]>sum)
-                    {
+                    else if(nums[lo]+nums[hi] > sum)
                         hi--;
-                    }
-                    
                 }
             }
         }
