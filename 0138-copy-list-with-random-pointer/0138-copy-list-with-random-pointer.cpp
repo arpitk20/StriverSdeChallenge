d936f9812dcc752e3fc72037a7f225daaaa06179
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-06-08 13:30:03 +0530

    Time: 7 ms (82.11%), Space: 11.2 MB (65.70%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,24 +17,63 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node*, Node*> mp;
+        //Using Extra Space
+//         map<Node*, Node*> mp;
         
-        Node* temp = head;
+//         Node* temp = head;
         
-        while(temp)
-        {
-            mp[temp] = new Node(temp->val);
-            temp = temp->next;//Mapping complete
+//         while(temp)
+//         {
+//             mp[temp] = new Node(temp->val);
+//             temp = temp->next;//Mapping complete
+//         }
+//         temp = head;
+        
+//         while(temp)
+//         {
+//             mp[temp]->next = mp[temp->next];
+//             mp[temp]->random = mp[temp->random];
+//             temp = temp->next;
+//         }
+        
+//         return mp[head];
+        
+        //Without Extra Spce
+        if (head == NULL) {
+            return NULL;
         }
-        temp = head;
-        
-        while(temp)
-        {
-            mp[temp]->next = mp[temp->next];
-            mp[temp]->random = mp[temp->random];
-            temp = temp->next;
+         
+        // Step 1: Create new nodes and insert them next to the original nodes
+        Node* curr = head;
+        while (curr != NULL) {
+            Node* newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = newNode->next;
         }
         
-        return mp[head];
+        // Step 2: Set the random pointers of the new nodes
+        curr = head;
+        while (curr != NULL) {
+            if (curr->random != NULL) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        
+        // Step 3: Separate the new nodes from the original nodes
+        curr = head;
+        Node* clonedHead = head->next;
+        Node* clonedCurr = clonedHead;
+        while (clonedCurr->next != NULL) {
+            curr->next = curr->next->next;
+            clonedCurr->next = clonedCurr->next->next;
+            curr = curr->next;
+            clonedCurr = clonedCurr->next;
+        }
+        curr->next = NULL;
+        clonedCurr->next = NULL;
+         
+        return clonedHead;
     }
 };
