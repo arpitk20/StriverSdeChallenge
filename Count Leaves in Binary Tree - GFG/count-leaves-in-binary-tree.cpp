ca53d4d21f9ba01f3bcceb197dafc526034a1078
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-06-19 12:50:18 +0530

    Added solution - LeetHub

--- Count Leaves in Binary Tree - GFG/count-leaves-in-binary-tree.cpp
+++ Count Leaves in Binary Tree - GFG/count-leaves-in-binary-tree.cpp
@@ -116,11 +116,11 @@ struct Node
    20       30 */
 int countLeaves(Node* root)
 {
+  // Your code here
     if(!root)
-        return 0;
-        
+      return 0;
     if(!root->left && !root->right)
         return 1;
         
-    return (countLeaves(root->left) + countLeaves(root->right));
+    return countLeaves(root->left) + countLeaves(root->right);
 }
