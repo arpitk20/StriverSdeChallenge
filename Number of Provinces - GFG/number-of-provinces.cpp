6de07e37c51c7c6bb3dd2d6424fe3929eb2370a2
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-07-04 11:18:37 +0530

    Added solution - LeetHub

---
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    void bfs(int vertex, vector<int> adjLs[], int vis[])
+    {
+        queue<int> q;
+        q.push(vertex);
+        vis[vertex] = 1;
+        while(!q.empty())
+        {
+            int temp = q.front();
+            q.pop();
+            for(auto x:adjLs[temp])
+            {
+                if(!vis[x])
+                {
+                    q.push(x);
+                    vis[x] = 1;
+                }
+            }
+        }
+        return;
+    }
+    int numProvinces(vector<vector<int>> adj, int V) {
+        
+        vector<int> adjLs[V]; 
+        
+        // to change adjacency matrix to list 
+        for(int i = 0;i<V;i++) {
+            for(int j = 0;j<V;j++) {
+                // self nodes are not considered
+                if(adj[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j); 
+                    adjLs[j].push_back(i); 
+                }
+            }
+        }
+        int vis[V] = {0}; 
+        int cnt = 0; 
+        for(int i = 0;i<V;i++) {
+            // if the node is not visited
+            if(!vis[i]) {
+                // counter to count the number of provinces 
+                cnt++;
+                bfs(i, adjLs, vis); 
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
