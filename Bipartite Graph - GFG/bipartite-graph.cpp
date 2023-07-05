84305694da71910157620065fca023d91e3c223e
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-07-05 16:45:40 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -8,28 +8,54 @@ public:
 	bool isBipartite(int V, vector<int>adj[]){
         
         vector<int> color(V, -1);
-		queue<int> q;
-		for (int i = 0; i < V; i++) {
-			if (color[i] == -1) {
-				q.push(i);
-				color[i] = 0;
-				while (!q.empty()) {
-					int node = q.front();
-					q.pop();
-					for (auto x : adj[node]) {
-						if (color[x] == color[node])
-							return false;
-						else if (color[x] == -1) {
-							q.push(x);
-							color[x] = 1 - color[node];
-						}
-					}
-				}
-			}
-		}
+        
+//BFS Solution
+// 		queue<int> q;
+// 		for (int i = 0; i < V; i++) {
+// 			if (color[i] == -1) {
+// 				q.push(i);
+// 				color[i] = 0;
+// 				while (!q.empty()) {
+// 					int node = q.front();
+// 					q.pop();
+// 					for (auto x : adj[node]) {
+// 						if (color[x] == color[node])
+// 							return false;
+// 						else if (color[x] == -1) {
+// 							q.push(x);
+// 							color[x] = 1 - color[node];
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+        for(int i=0;i<V;i++)
+        {
+            if(color[i]==-1)
+            {
+                color[i] = 0;
+                if(dfs(i, color, adj)==false)
+                    return false;
+            }
+        }
 		return true;
 	}
+	bool dfs(int node, vector<int>& color, vector<int> adj[])
+	{
 
+	    for(auto x:adj[node])
+	    {
+	        if(color[x]==color[node])
+	            return false;
+	        else if(color[x]==-1)
+	        {
+	            color[x] = (color[node]+1)%2;
+	            if(dfs(x, color, adj)==false)
+	                return false;
+	        }
+	    }
+	    return true;
+	}
 };
 
 //{ Driver Code Starts.
