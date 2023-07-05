ec27af2f4f3c41573935878204ad715faa264139
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-07-05 15:54:00 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,45 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    bool check(int start, int V, vector<int>adj[], int color[]) {
-        queue<int> q;
-	    q.push(start); 
-	    color[start] = 0; 
-	    while(!q.empty()) {
-	        int node = q.front();
-	        q.pop(); 
-	        
-	        for(auto it : adj[node]) {
-	            // if the adjacent node is yet not colored
-	            // you will give the opposite color of the node 
-	            if(color[it] == -1) {
-	                
-	                color[it] = !color[node]; 
-	                q.push(it); 
-	            }
-	            // is the adjacent guy having the same color 
-	            // someone did color it on some other path 
-	            else if(color[it] == color[node]) {
-	                return false; 
-	            }
-	        }
-	    }
-	    return true; 
-    }
 	bool isBipartite(int V, vector<int>adj[]){
         
-        int color[V]; 
-	    for(int i = 0;i<V;i++) color[i] = -1; 
-	    
-	    for(int i = 0;i<V;i++) {
-	        // if not coloured
-	        if(color[i] == -1) {
-	            if(check(i, V, adj, color) == false) {
-	                return false; 
-	            }
-	        }
-	    }
-	    return true; 
+        vector<int> color(V, -1);
+		queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (color[i] == -1) {
+				q.push(i);
+				color[i] = 0;
+				while (!q.empty()) {
+					int node = q.front();
+					q.pop();
+					for (auto x : adj[node]) {
+						if (color[x] == color[node])
+							return false;
+						else if (color[x] == -1) {
+							q.push(x);
+							color[x] = 1 - color[node];
+						}
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 };
