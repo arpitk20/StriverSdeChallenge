da8d2879254f9cdce96d81092e3b975f9bea9b71
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-07-10 15:34:06 +0530

    Added solution - LeetHub

--- Number of Ways to Arrive at Destination - GFG/number-of-ways-to-arrive-at-destination.cpp
+++ Number of Ways to Arrive at Destination - GFG/number-of-ways-to-arrive-at-destination.cpp
@@ -6,52 +6,43 @@ using namespace std;
 // User function Template for C++
 class Solution {
 public:
-    int countPaths(int n, vector<vector<int>>& grid) {
-        vector <vector <pair <long long, long long>>> graph(n+1);
-        for(auto x: grid) { //creating adjacency list
-            graph[x[0]].push_back({x[1], x[2]});
-            graph[x[1]].push_back({x[0], x[2]});
+    int countPaths(int n, vector<vector<int>>& roads) {
+        int mod = 1e9+7;
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto &road: roads) {
+            graph[road[0]].push_back({road[1], road[2]});
+            graph[road[1]].push_back({road[0], road[2]});
         }
-        long long INF = 1e15;
-        long long MOD = 1e9 + 7;
-        vector <long long> dp(n+1, 0);
-        vector <long long> dist(n+1, INF);
-        priority_queue <pair <long long, long long>> q; // maxHeap in c++
-		// although we can use use minHeap in c++ by adding parameter to the maxHeap template
-		// but I have inserted -1*val into the maxHeap so it worked fine as minHeap
-		
-        dist[0] = 0;
-        dp[0] = 1; // initilize it to 1 since to reach 0 we have 1 way
-        q.push({0, 0});
-		
-        while(q.size()) {
-            long long node = q.top().second;
-            long long nodeWt = -q.top().first;
-            q.pop();
-
-			/*
-			what if we inserted min weight after some greater weight and we always pop min weight so it can be
-			possible that some nodeWt (> dist[node]) is there in the queue that we haven't popped yet...
-			hence we are continuing so that we don't traverse adjacency list of node again
-			
-			I have taken a graph as an example below the code.
-			*/
-            if(dist[node] < nodeWt) continue; // explained above
-            for(auto key: graph[node]) {
-                long long child = key.first;
-                long long childWt = key.second;
-                if(nodeWt + childWt == dist[child]) {
-					// we are adding parent's dp value to child's dp 
-                    dp[child] = (dp[child] + dp[node])%MOD;
+        
+        vector<long long> distance(n, LONG_MAX);
+        vector<int> path(n, 0);
+        
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, 0});
+        distance[0] = 0;
+        path[0] = 1;
+        
+        while(!pq.empty()) {
+            pair<long long, int> t = pq.top();
+            pq.pop();
+            
+            for(auto &nbr: graph[t.second]) {
+                long long vert = nbr.first;
+                long long edge = nbr.second;
+                
+                if(distance[vert] > distance[t.second] + edge) {
+                    distance[vert] = distance[t.second] + edge;
+                    pq.push({distance[vert], vert});
+                    path[vert] = path[t.second] %mod;
                 }
-                if(nodeWt + childWt < dist[child]) {
-                    dp[child] = dp[node]%MOD; // we are getting value of parent's dp
-                    dist[child] = nodeWt + childWt;
-                    q.push({-dist[child], child});
+                else if(distance[vert] == t.first + edge) {
+                    path[vert] += path[t.second];
+                    path[vert] %= mod;
                 }
             }
         }
-        return dp[n-1];
+        
+        return path[n-1];
     }
 };
 
