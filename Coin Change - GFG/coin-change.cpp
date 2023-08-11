07e55f2a74d2984b06ac471c69aedc6dbf72e3a8
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-11 14:41:59 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -9,17 +9,17 @@ class Solution {
     long long int count(int coins[], int n, int sum) {
 
         ll dp[n+1][sum+1];
-        for(int i=0;i<n+1;i++)
-            dp[i][0] = 1;
         for(int i=0;i<sum+1;i++)
             dp[0][i] = 0;
+        for(int i=0;i<n+1;i++)
+            dp[i][0] = 1;
         
         for(int i=1;i<n+1;i++)
         for(int j=1;j<sum+1;j++)
         {
             if(coins[i-1]<=j)
             {
-                dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]; 
             }
             else
             {
