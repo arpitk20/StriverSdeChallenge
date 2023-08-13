3cff57b8178294fb2c68a54363f3f7cd057a02d8
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-13 19:25:24 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (92.28%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,65 +1,29 @@
 class Solution {
 public:
-    void helper(int i , vector<int> &subset , vector<int> &nums , vector<vector<int>> &ans)
+    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& output, int index)
     {
-        // When we have reached the end of nums[] , we have generated a Valid Unique Subset for sure
-        
-        if(i == nums.size() )
+        if(index==nums.size())
         {
-            ans.push_back(subset) ;
-            return ;
+            ans.push_back(output);
+            return;
         }
+        output.push_back(nums[index]);
+        solve(nums, ans, output, index+1);
+        output.pop_back();
         
-        // Otherwise we will use our Pick & Don't Pick Logic
-        
-        // Consider the Element
-        
-        subset.push_back(nums[i]) ;
-        
-        // Ask Recursion to do rest of the task
-        
-        helper(i + 1 , subset , nums , ans) ;
-        
-        // Backtrack and undo the change you have done
-        
-        subset.pop_back() ;
-        
-        // While using Don't Pick option, we must ensure we skip all the Duplicate Occurrences of nums[i]
-        
-        while(i < nums.size() - 1 && (nums[i] == nums[i + 1] ) )
-        {
-            i++ ;
-        }
-        
-        // Our i pointer will stop on the last Duplciate Occurencce of nums[i]
-        
-        // Don't Consider the Element and Ask Recursion to generate Unique Subsets
-        
-        helper(i + 1 , subset , nums , ans) ;
-        
-        return ;
+        //avoiding the same elements
+        while(index<nums.size()-1 && (nums[index]==nums[index+1]))
+            index++;
+        solve(nums, ans, output, index+1);
     }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
-    public:
-        vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-         
-        // For our logic to work, we need to sort the given nums[] vector
-            
-        sort(nums.begin() , nums.end() ) ;
-            
-        vector<int>subset ;
-            
-        vector<vector<int> > ans ;
-            
-        // Call the helper() function to generate all Unique Subsets
-            
-        helper(0 , subset , nums , ans) ;
-            
-        return ans ;
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        //we need to sort for comparison in solve function
+        sort(nums.begin(), nums.end());
+        solve(nums, ans, output, index);
+        return ans;
     }
-    };
-
-/*
-Time Complexity  : O(2^N)
-Space Complexity : O(N)
-*/
+};
