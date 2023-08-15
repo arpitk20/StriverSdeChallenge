937291ff6b472e88c777ed82ed3741b2d0b20394
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-15 23:15:28 +0530

    Time: 101 ms (84.53%), Space: 57.8 MB (36.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    bool isPallindrome(string& s, int start, int end)
+    bool isPallindrome(string s, int i, int j)
     {
-        while(start<=end)
+        while(i<j)
         {
-            if(s[start++]!=s[end--])
+            if(s[i]!=s[j])
                 return false;
-            else
-                continue;
+            i++;
+            j--;
         }
         return true;
     }
-    void solve(string& s, vector<vector<string>>& res, vector<string>& output, int index)
+    void solve(string s, vector<vector<string>> &ans, vector<string> &temp, int index)
     {
         if(index>=s.size())
-            res.push_back(output);
+        {
+            ans.push_back(temp);
+            return;
+        }
         for(int i=index;i<s.size();i++)
         {
             if(isPallindrome(s, index, i))
             {
-                output.push_back(s.substr(index, i-index+1));
-                solve(s, res, output, i+1);
-                output.pop_back();
+                temp.push_back(s.substr(index, i-index+1));
+                solve(s, ans, temp, i+1);
+                temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        int n = s.size();
-        vector<vector<string>> res;
-        vector<string> output;
-        int index = 0;
-        solve(s, res, output, index);
-        return res;
+        
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(s, ans, temp, 0);
+        return ans;
     }
 };
