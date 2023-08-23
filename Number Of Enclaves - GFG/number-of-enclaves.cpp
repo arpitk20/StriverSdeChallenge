0e7a5558c2335df6d9894934c13d3468f05d7bd6
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-23 17:57:16 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,58 +10,59 @@ using namespace std;
 
 class Solution {
   public:
-    int numberOfEnclaves(vector<vector<int>> &grid) 
+    void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid)
     {
+        queue<pair<int, int>> q;
+        int drow[4] = {-1, 0, 1, 0};
+        int dcol[4] = {0, -1, 0, 1};
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-        {
-            if(i==0||i==n-1||j==0||j==m-1)
-            {
-                if(grid[i][j]==1)//it's a land
-                {
-                    q.push({i, j});
-                    vis[i][j] = 1;
-                }
-            }
-        }
-        
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, -1, 0, 1};
-        
+        if(vis[row][col])
+            return;
+        vis[row][col] = 1; 
+        q.push({row, col});
         while(!q.empty())
         {
-            int row = q.front().first;
-            int col = q.front().second;
+            row = q.front().first;
+            col = q.front().second;
             q.pop();
-            
             for(int i=0;i<4;i++)
             {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-                && !vis[nrow][ncol] && grid[nrow][ncol]==1)
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow>=0&&ncol>=0&&nrow<n&&ncol<m&&!vis[nrow][ncol]&&grid[nrow][ncol]==1)
                 {
-                    vis[nrow][ncol]=1;
+                    vis[nrow][ncol] = 1;
                     q.push({nrow, ncol});
                 }
             }
         }
+        return;
+    }
+    int numberOfEnclaves(vector<vector<int>> &grid) {
         
-        int cnt = 0;
-        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         for(int i=0;i<n;i++)
+        {
+            if(!vis[i][0] && grid[i][0]==1)
+                bfs(i, 0, vis, grid);
+            if(!vis[i][m-1] && grid[i][m-1]==1)
+                bfs(i, m-1, vis, grid);
+        }
         for(int j=0;j<m;j++)
         {
-            if(vis[i][j]==0 && grid[i][j]==1)
-            {
-                cnt++;
-            }
+            if(!vis[0][j] && grid[0][j]==1)
+                bfs(0, j, vis, grid);
+            if(!vis[n-1][j] && grid[n-1][j]==1)
+                bfs(n-1, j, vis, grid);
         }
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            if(!vis[i][j] && grid[i][j]==1)
+                cnt++;
         return cnt;
     }
 };
