63783c07b4fc05ffff0bd1e996fe58cc5a69e807
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-25 18:22:51 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,85 +10,50 @@ using namespace std;
 
 class Solution {
   public:
-    bool dfsCheck(int node, int vis[], int pathVis[], vector<int> adj[], int check[])
+    bool dfs(int i, vector<int> adj[], vector<int> &vis)
     {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        for(auto it:adj[node])
+        vis[i] = 1;
+        
+        for(auto n : adj[i])
         {
-            if(!vis[it])
+            if(vis[n] == 0)
             {
-                if(dfsCheck(it, vis, pathVis, adj, check)==true)
-                {
-                    check[node] = 0;//definitely not a safe node since a cycle
-                    return true;
-                }
+                if(dfs(n, adj, vis)) return true;
             }
-            //if the node has been visited and has been visited on the same path
-            else if(vis[it] && pathVis[it])
-            {
-                check[node] = 0;
-                return true;
-            }
+            else if (vis[n] == 1) return true;
         }
-        check[node] = 1;//dfs is complete means no cycle
-        pathVis[node] = 0;
+        
+        vis[i] = 2;
         return false;
     }
-    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
     
-//DFS SOLUTION
-
-        //can be solved using cycle detect or topological sort
-        // int vis[V] = {0};
-        // int pathVis[V] = {0};
-        // int check[V] = {0};//to mark safe nodes
-        // vector<int> safeNodes;
+  public:
+    // Function to detect cycle in a directed graph.
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) 
+    {
+        /*
+            0 -> unvisited node
+            1 -> visited node
+            2 -> visited previously in current path
+            3 -> safe node
+        */
+        vector<int> vis (V, 0);
+        vector<int> ans;
         
-        // for(int i=0;i<V;i++)
-        // {
-        //     if(!vis[i])
-        //     {
-        //         dfsCheck(i, vis, pathVis, adj, check);
-        //     }
-        // }
-        // for(int i=0;i<V;i++)
-        //     if(check[i]==1)
-        //         safeNodes.push_back(i);
-
-//BFS TOPOLOGICAL SORT SOLUTION
-        vector<int> safeNodes;
-        //we reverse the graph and insert the appropriate nodes
-        vector<int> adjRev[V];
-        for(int i=0;i<V;i++)
+        for(int i = 0; i < V; i++)
         {
-            for(auto x:adj[i])
-                adjRev[x].push_back(i);
-        }
-        int inDegree[V] = {0};
-        for(int i=0;i<V;i++)
-        {
-            for(auto x:adjRev[i])
-                inDegree[x]++;
-        }
-        queue<int> q;
-        for(int i=0;i<V;i++)
-            if(inDegree[i]==0)
-                q.push(i);
-        while(!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            safeNodes.push_back(node);
-            for(auto x:adjRev[node])
+            if(vis[i] == 0) 
             {
-                inDegree[x]--;
-                if(inDegree[x]==0)
-                    q.push(x);
+                dfs(i, adj, vis);
             }
         }
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        
+        for(int i = 0; i < V; i++)
+        {
+            if(vis[i] == 2) ans.push_back(i);
+        }
+        
+        return ans;
     }
 };
 
