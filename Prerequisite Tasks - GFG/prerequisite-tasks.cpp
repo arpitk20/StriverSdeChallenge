28274e89e706e871a8ee3f2be6c1c5ba4603483c
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-27 07:24:03 +0530

    Added solution - LeetHub

--- Prerequisite Tasks - GFG/prerequisite-tasks.cpp
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -5,45 +5,40 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-	bool isPossible(int V, vector<pair<int, int> >& prerequisites) {
-        
-        vector<int> adj[V];
-        for(auto x:prerequisites)
-        {
-            adj[x.first].push_back(x.second);
-        }
-        
-        queue<int> q;
-        int inDegree[V] = {0};
-        for(int i=0;i<V;i++)
-        {
-            for(auto x:adj[i])
-                inDegree[x]++;
-        }
-        for(int i=0;i<V;i++)
-        {
-            if(inDegree[i]==0)
-                q.push(i);
-        }
-        
-        vector<int> topo;
-        while(!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto x:adj[node])
-            {
-                inDegree[x]--;
-                if(inDegree[x]==0)
-                    q.push(x);
-            }
-        }
-        
-        //now if there is a cycle then we can't complete all our tasks
-        if(topo.size()==V)//as there is no cycle
-            return true;
-        return false;
+	bool isPossible(int N,int P, vector<pair<int, int> >& prerequisites) {
+	    
+	    vector<int> adj[N];
+	    for(auto x:prerequisites)
+	    {
+	        adj[x.second].push_back(x.first);
+	    }
+	    
+	    vector<int> topo;
+	    int inDegree[N] = {0};
+	    for(int i=0;i<N;i++)
+	    {
+	        for(auto x:adj[i])
+	            inDegree[x]++;
+	    }
+	    queue<int> q;
+	    for(int i=0;i<N;i++)
+	    {
+	        if(inDegree[i]==0)
+	            q.push(i);
+	    }
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        for(auto x:adj[node])
+	        {
+	            inDegree[x]--;
+	            if(inDegree[x]==0)
+	                q.push(x);
+	        }
+	    }
+	    return (topo.size()==N);
 	}
 };
 
@@ -64,7 +59,7 @@ int main(){
         // string s;
         // cin>>s;
         Solution ob;
-        if (ob.isPossible(N, prerequisites))
+        if (ob.isPossible(N,P, prerequisites))
             cout << "Yes";
         else
             cout << "No";
