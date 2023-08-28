31cbf6543ac5a7a19d9628dc9a109ce0de0a4ca2
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-28 21:32:09 +0530

    Added solution - LeetHub

--- Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
+++ Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
@@ -5,41 +5,40 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    int drow[4] = {-1, 0, 1, 0};
+    int dcol[4] = {0, -1, 0, 1};
     int MinimumEffort(vector<vector<int>>& heights) {
         
         int n = heights.size();
         int m = heights[0].size();
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>> pq;//absolute dist, row, col
-        //min heap because whenever we reach the source, we'll reach via tha path which 
-        //has the minimum difference of all the maximum heights
-        vector<vector<int>> effort(n, vector<int>(m, 1e9));//effort from source to cell
-        effort[0][0] = 0;
-        pq.push({0, {0, 0}});
+        greater<pair<int, pair<int, int>>>> pq;
         
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, -1, 0, 1};
+        vector<vector<int>> effort(n, vector<int>(m, 1e9));
+        pq.push({0, {0, 0}});//effort, row, col
+        effort[0][0] = 0;
+        
         while(!pq.empty())
         {
-            int diff = pq.top().first;//maximum difference for that particular path
+            int diff = pq.top().first;
             int row = pq.top().second.first;
             int col = pq.top().second.second;
             pq.pop();
-            if(row==n-1 && col==m-1)
-                return diff;
             
+            if(row==n-1 && col==m-1)
+                return effort[row][col];
+                
             for(int i=0;i<4;i++)
             {
-                int nrow = row+delrow[i];
-                int ncol = col+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m)
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m)
                 {
-                    int newEffort = max(abs(heights[nrow][ncol]-heights[row][col]), diff);
-                    //stores maximum effort till now
+                    int newEffort = max(diff, abs(heights[nrow][ncol]-heights[row][col]));
                     if(newEffort<effort[nrow][ncol])
                     {
-                        pq.push({newEffort, {nrow, ncol}});
                         effort[nrow][ncol] = newEffort;
+                        pq.push({effort[nrow][ncol], {nrow, ncol}});
                     }
                 }
             }
