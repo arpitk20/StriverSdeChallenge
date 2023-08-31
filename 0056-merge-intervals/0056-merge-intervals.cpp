b1ad249e96c85c38b5119b5758a9d2205c8d3f1a
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-08-31 11:44:19 +0530

    Time: 32 ms (67.53%), Space: 19 MB (44.92%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
+        ans.push_back(intervals[0]);
+        for(int i=0;i<n;i++)
+        {
+            if(ans.back()[1] >= intervals[i][0])//compare the last and the first value
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            else
+                ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
