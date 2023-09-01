259f0c9799531e90dd6bf6badae485968b616db0
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-09-01 13:41:10 +0530

    Time: 3 ms (39.61%), Space: 6.1 MB (28.68%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    double myPow(double x, long long n) {
+    double myPow(double x, int n) {
         
-        if(n==0)
-            return 1.0;
-        
-        if(n<0)
-            return (1.0/pow(x, -n));
-        
-        double temp = myPow(x, n/2);
-        
-        if(n%2==0)
-            return temp*temp;
-        
-        else if(n%2==1)
-            return x*temp*temp;
-        
-        return 1.0;
+        double ans = 1.0;
+        while(n)
+        {
+            if(n%2)
+                ans = (n>0)?ans*x:ans/x;
+            x = x*x;
+            n /= 2;
+        }
+        return ans;
     }
 };
