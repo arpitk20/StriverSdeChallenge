7dd3287005f5867e1a4ca902adef2fab1e4ef900
arpitk20 <84130362+arpitk20@users.noreply.github.com>
2023-09-12 11:31:40 +0530

    Time: 16 ms (88.52%), Space: 16.2 MB (57.50%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,50 @@
 class MinStack {
 public:
-    stack<int> s1, s2;//s1 for storing elements, s2 for storing minimum elements
+    stack<long> s;
+    long mn;
     MinStack() {
         
     }
     
-    void push(int val) {
-        s1.push(val);
-        if(s2.empty()||getMin()>=val)
-            s2.push(val);
+    void push(int x) {
+        if(s.empty()){
+            s.push(x);
+            mn = x;
+        }
+        else if(x <= mn){
+            // put 2*x - mn into stack
+            s.push(2ll*x - mn);
+            mn = x;
+        }
+        else{
+            s.push(x);
+        }
     }
     
     void pop() {
-        if(s2.top()==s1.top())
-            s2.pop();
-        s1.pop();
+        if(s.empty()) return;
+        // stack top can never be smaller than mn, if it is then it indicates a flag which means before popping, you have to update mn
+        if(s.top() < mn){
+            // mn = 2*mn - s.top()
+            mn = 2ll*mn - s.top();
+        }
+        s.pop();
     }
     
+    // critical part
     int top() {
-        return s1.top();
+        if(s.empty()) return -1;
+        if(s.top() < mn){
+            return mn;
+        }
+        return s.top();
     }
     
     int getMin() {
-        return s2.top();
+        if(s.empty()) return -1;
+        return mn;
     }
+    
 };
 
 /**
